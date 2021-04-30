f2f5e28019649ad2c4159bea2537731319029067
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2021-04-30 11:37:42 +0200

    initial stuff

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
  * @package _s
  */
 
-get_header();
+get_header('minimal');
 ?>
 
 	<main id="primary" class="site-main">
@@ -18,18 +18,7 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', '_s' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', '_s' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
+			
 		endwhile; // End of the loop.
 		?>
 
---
+++ template-parts/performance-canvas.php
@@ -0,0 +1,39 @@
+<div class="triptico_performance_header">
+	<?php 
+	$datestart = get_post_meta($post->ID, '_tri_start_perfo', true);
+	$dateend = get_post_meta($post->ID, '_tri_end_perfo', true);
+	$duracion = get_post_meta($post->ID, '_tri_length_perfo', true);
+	?>	
+	
+	<p>
+		<strong><?php echo $post->post_title;?></strong> - <strong><?php echo date('d/m/Y - H:i(e)', $datestart);?></strong> - Duración: <strong><?php echo $duracion;?> minutos</strong>
+	</p>
+
+</div>
+<div id="triptico_performance_wrapper">
+	<div id="triptico_performance_visualization">
+		<canvas id="triptico_canvas">
+			
+		</canvas>
+		<div id="triptico_canvas_left" class="triptico_box">
+			<div class="infoleft">
+			<strong>Mano izquierda</strong>
+			<div id="infozones_left">
+			</div>		
+		</div>
+		</div>
+		<div class="centerzone">
+			<div id="triptico_canvas_3d" class="triptico_box"></div>
+			<div id="triptico_canvas_rug" class="triptico_box">
+				<img src="" alt="IndexP" id="image_performance">
+			</div>
+		</div>
+		<div id="triptico_canvas_right" class="triptico_box">
+			<div class="inforight">
+				<strong>Mano derecha</strong>
+				<div id="infozones_right">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
